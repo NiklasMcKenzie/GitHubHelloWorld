4945157dc5466b4c6332d448287246321e65a292
Niklas McKenzie <niklas.mcenzie@gmail.com>
2017-01-19 09:18:21 +0100

    Added Hello world

---
+++ GitHubHelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyProgram myProgram;
+            myProgram = new MyProgram();
+            myProgram.Run();
+
+        }
+    }
+}
